a72fd79f524cbc6438dbe816c4c812e027641596
Eugen Meltis <meltiseugen@gmail.com>
2026-03-02 11:00:12 +0200

    FIx for elite rare not shoing rare icon

--- Display/EliteMarker.lua
+++ Display/EliteMarker.lua
@@ -17,27 +17,45 @@ end
 
 function addonTable.Display.EliteMarkerMixin:SetUnit(unit)
   self.unit = unit
+  self:UnregisterAllEvents()
   if self.unit then
-    if self.details.openWorldOnly and addonTable.Display.Utilities.IsInRelevantInstance() then
-      self.marker:Hide()
-      return
-    end
-
-    local classification = UnitClassification(self.unit)
-    if classification == "elite" or classification == "worldboss" then
-      self.marker:Show()
-      self.marker:SetTexture(self.eliteTexture)
-    elseif classification == "rareelite" then
-      self.marker:Show()
-      self.marker:SetTexture(self.rareEliteTexture)
-    else
-      self.marker:Hide()
-    end
+    self:RegisterUnitEvent("UNIT_CLASSIFICATION_CHANGED", self.unit)
+    self:Update()
+  else
+    self.marker:Hide()
   end
 end
 
 function addonTable.Display.EliteMarkerMixin:Strip()
+  self:UnregisterAllEvents()
   self.eliteTexture = nil
   self.rareEliteTexture = nil
   self.PostInit = nil
+end
+
+function addonTable.Display.EliteMarkerMixin:Update()
+  if not self.unit then
+    self.marker:Hide()
+    return
+  end
+
+  if self.details.openWorldOnly and addonTable.Display.Utilities.IsInRelevantInstance() then
+    self.marker:Hide()
+    return
+  end
+
+  local classification = UnitClassification(self.unit)
+  if classification == "elite" or classification == "worldboss" then
+    self.marker:Show()
+    self.marker:SetTexture(self.eliteTexture)
+  elseif classification == "rareelite" then
+    self.marker:Show()
+    self.marker:SetTexture(self.rareEliteTexture)
+  else
+    self.marker:Hide()
+  end
+end
+
+function addonTable.Display.EliteMarkerMixin:OnEvent(eventName, ...)
+  self:Update()
 end
--- Display/RareMarker.lua
+++ Display/RareMarker.lua
@@ -5,17 +5,30 @@ addonTable.Display.RareMarkerMixin = {}
 
 function addonTable.Display.RareMarkerMixin:SetUnit(unit)
   self.unit = unit
+  self:UnregisterAllEvents()
   if self.unit then
-    local classification = UnitClassification(self.unit)
-    if classification == "rare" or (self.details.includeElites and classification == "rareelite") then
-      self.marker:Show()
-    else
-      self.marker:Hide()
-    end
+    self:RegisterUnitEvent("UNIT_CLASSIFICATION_CHANGED", self.unit)
+    self:Update()
   else
     self:Strip()
   end
 end
 
+function addonTable.Display.RareMarkerMixin:Update()
+  if not self.unit then
+    self.marker:Hide()
+    return
+  end
+
+  local classification = UnitClassification(self.unit)
+  local show = classification == "rare" or (self.details.includeElites and classification == "rareelite")
+  self.marker:SetShown(show)
+end
+
 function addonTable.Display.RareMarkerMixin:Strip()
+  self:UnregisterAllEvents()
+end
+
+function addonTable.Display.RareMarkerMixin:OnEvent(eventName, ...)
+  self:Update()
 end
